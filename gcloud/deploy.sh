9f47c92819d5e41277d49a54d408a2ef98bd4449
Yura Bozhyk <yura.bozhyk@gmail.com>
2019-05-18 19:30:58 +0300

    (Yura) Trying to complete deploying images on GCP

---
+++ gcloud/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+docker build -t gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}:$TRAVIS_COMMIT -f script/Dockerfile .
+docker build -t gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:$TRAVIS_COMMIT -f site/Dockerfile .
+
+echo $GCLOUD_SERVICE_KEY_TEST | base64 --decode -i > ${HOME}/client-secret.json
+gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
+
+gcloud --quiet config set project $PROJECT_PROD
+gcloud --quiet config set container/cluster $CLUSTER
+gcloud --quiet config set compute/zone ${ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER
+
+gcloud docker -- push gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}
+gcloud docker -- push gcr.io/${PROJECT_PROD}/${SITE_IMAGE}
+
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}:latest
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:latest
