7b4178e8ce6016261ce59f760eff1f186c25b056
Yura Bozhyk <yura.bozhyk@gmail.com>
2019-05-18 22:32:40 +0300

    Deploying test

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -9,9 +9,9 @@ echo $GCLOUD_SERVICE_KEY_TEST | base64 --decode -i > ${HOME}/client-secret.json
 gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
 
 gcloud --quiet config set project $PROJECT_PROD
-gcloud --quiet config set container/cluster $CLUSTER
+# gcloud --quiet config set container/cluster $CLUSTER
 gcloud --quiet config set compute/zone ${ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER
+# gcloud --quiet container clusters get-credentials $CLUSTER
 
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${SITE_IMAGE}
