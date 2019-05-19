e08440c45922592e09415e6e382983438f6bb068
Dima <dima.lavrishin@gmail.com>
2019-05-20 02:21:56 +0300

    try without get-credentials

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -10,8 +10,6 @@ docker tag eclipse-mosquitto gcr.io/${PROJECT_PROD}/${MQTT_IMAGE}:$TRAVIS_COMMIT
 gcloud --quiet config set project $PROJECT_PROD
 gcloud --quiet config set container/cluster $CLUSTER
 gcloud --quiet config set compute/zone ${ZONE}
-gcloud --quiet components install kubectl
-gcloud --quiet container clusters get-credentials $CLUSTER
 
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}
 gcloud docker -- push gcr.io/${PROJECT_PROD}/${SITE_IMAGE}
@@ -22,9 +20,6 @@ yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:latest
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${MONGO_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${MONGO_IMAGE}:latest
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_PROD}/${MQTT_IMAGE}:$TRAVIS_COMMIT gcr.io/${PROJECT_PROD}/${MQTT_IMAGE}:latest
-
-kubectl config view
-kubectl config current-context
 
 kubectl set image deployment/${SITE_DEPLOYMENT} ${SITE_CONTAINER}=gcr.io/${PROJECT_PROD}/${SITE_IMAGE}:$TRAVIS_COMMIT
 kubectl set image deployment/${SCRIPT_DEPLOYMENT} ${SCRIPT_CONTAINER}=gcr.io/${PROJECT_PROD}/${SCRIPT_IMAGE}:$TRAVIS_COMMIT
