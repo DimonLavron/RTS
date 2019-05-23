9ef530033160f9abe2a7311aa38f797effb015bf
Yura Bozhyk <yura.bozhyk@gmail.com>
2019-05-23 18:55:12 +0300

    Added check for unique id when adding new runner

--- site/app/forms.py
+++ site/app/forms.py
@@ -1,6 +1,10 @@
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from app import db
+
+runners_col = db.runners
 
 
 class RegisterRunnerForm(FlaskForm):
@@ -8,6 +12,11 @@ class RegisterRunnerForm(FlaskForm):
     last_name = StringField('Last Name', validators=[DataRequired()])
     id = StringField('ID', validators=[DataRequired()])
     submit = SubmitField('Register Runner')
+
+    def validate_id(self, id):
+        list = runners_col.distinct(key='id')
+        if id.data in list:
+            raise ValidationError('Please use a different id.')
 
 
 class RegisterCheckpointForm(FlaskForm):
