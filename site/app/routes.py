d9eb2f33cef1a2c793d2e4ae0e5cb327ac2f3cd3
Yura Bozhyk <yura.bozhyk@gmail.com>
2019-06-03 02:13:41 +0300

    Added UI for login, register forms; tables, header of pages

--- site/app/routes.py
+++ site/app/routes.py
@@ -17,6 +17,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	title = "Login"
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 	form = LoginForm()
@@ -42,7 +43,7 @@ def login():
 		else:
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
-	return render_template('login.html', form=form)
+	return render_template('login.html', form=form, title=title)
 
 
 @mqtt.on_connect()
@@ -80,13 +81,15 @@ def clear_checkpoints(race_id):
 
 @app.route("/results")
 def table():
+	title = "Results"
 	list = [event for event in events_col.find()]
 	list.reverse()
-	return render_template("table.html", events=list)
+	return render_template("table.html", events=list, title=title)
 
 
 @app.route('/register_runner', methods=['GET', 'POST'])
 def register_runner():
+	title = "Runner Registration"
 	if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	form = RegisterRunnerForm()
@@ -95,15 +98,17 @@ def register_runner():
 		runners_col.insert_one(runner.__dict__)
 		flash('Runner {} {} is successfully registered.'.format(runner.first_name, runner.last_name))
 		return redirect(url_for('runners_table'))
-	return render_template('register_runner.html', form=form)
+	return render_template('register_runner.html', form=form, title=title)
 
 @app.route('/runners')
 def runners_table():
+	title = "Runners"
 	list = [runner for runner in runners_col.find()]
-	return render_template('runners_table.html', runners=list)
+	return render_template('runners_table.html', runners=list, title=title)
 
 @app.route('/register_race', methods=['GET', 'POST'])
 def register_race():
+	title = "Race Registration"
 	if current_user.is_anonymous:
 		return redirect(url_for('index'))
 	form = RegisterRaceForm()
@@ -115,12 +120,13 @@ def register_race():
 		races_col.insert_one(race.__dict__)
 		flash('Race {} is successfully registered.'.format(race.name))
 		return redirect(url_for('races'))
-	return render_template('register_race.html', form=form)
+	return render_template('register_race.html', form=form, title=title)
 
 @app.route("/races")
 def races():
+	title = "Races"
 	list = [race for race in races_col.find()]
-	return render_template("races.html", races=list)
+	return render_template("races.html", races=list, title=title)
 
 @app.route("/race/<race_id>")
 def race(race_id):
